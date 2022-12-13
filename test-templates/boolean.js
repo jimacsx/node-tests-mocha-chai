bc1635c3dca6484f7edc99ede14a998b8f0b9330
jimacsx <maximino.peralta5791@gmail.com>
2022-12-12 19:13:09 -0600

    Sección de la Introducción de mocha y chai para los tests - Finalizado

---
+++ test-templates/boolean.js
@@ -0,0 +1,8 @@
+const expect = require("chai").expect;
+describe.only("Boolean test", () => {
+  it("El valor true es verdadero", () => {});
+  it("El valor false es falso", () => {});
+  it("1 es de tipo número", () => {});
+  it("1 NO es de tipo string", () => {});
+  it("1 NO es de tipo booleano", () => {});
+});
---
+++ test-templates/strings.js
@@ -0,0 +1,8 @@
+const expect = require("chai").expect;
+describe.only("Strings test", () => {
+  it("Es igual al valor del string", () => {});
+  it("Es de tipo string", () => {});
+  it("Tiene una longitud de 3 carácteres", () => {});
+  it('Encuentra / Incluye "ba" en la palabra', () => {});
+  it('NO incluye "fa" en la palabra', () => {});
+});
